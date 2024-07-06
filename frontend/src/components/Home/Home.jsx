97eec4ccbc158975000a9a4bdcc74eabd73fb9b0
nasir hanif <101257760+NASIR-HANIF@users.noreply.github.com>
2024-07-06 14:09:58 +0500

    frontend changes

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -112,13 +112,13 @@ const Home = ({ timelines, skills, youtubes=[] }) => {
       camera.rotation.z = window.scrollY * 0.001;
       camera.rotation.y = window.scrollY * 0.003;
 
-      const skillsBox = document.getElementById("homeskillsBox");
+      // const skillsBox = document.getElementById("homeskillsBox");
 
-      if (window.scrollY > 1500) {
-        skillsBox.style.animationName = "homeskillsBoxAnimationOn";
-      } else {
-        skillsBox.style.animationName = "homeskillsBoxAnimationOff";
-      }
+      // if (window.scrollY > 1500) {
+      //   skillsBox.style.animationName = "homeskillsBoxAnimationOn";
+      // } else {
+      //   skillsBox.style.animationName = "homeskillsBoxAnimationOff";
+      // }
     });
   }, []);
 

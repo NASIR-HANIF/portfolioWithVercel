c4fc477a0f5e2d9a987cf0fe42ea467abd6d0d3b
nasir hanif <101257760+NASIR-HANIF@users.noreply.github.com>
2024-09-18 11:09:39 +0500

    Update TimeLine.jsx

--- frontend/src/components/TimeLine/TimeLine.jsx
+++ frontend/src/components/TimeLine/TimeLine.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import Timeline from "@mui/lab/Timeline"
-import TimelineItem from "@mui/lab/TimelineItem"
+import Timeline from "@mui/lab/Timeline";
+import TimelineItem from "@mui/lab/TimelineItem";
 import TimelineSeparator from "@mui/lab/TimelineSeparator";
-import TimelineConnector from "@mui/lab/TimelineConnector"
-import TimelineContent from "@mui/lab/TimelineContent"
-import TimelineOppositeContent from "@mui/lab/TimelineOppositeContent"
-import TimelineDot from "@mui/lab/TimelineDot"
-import { Event } from "@mui/icons-material"
+import TimelineConnector from "@mui/lab/TimelineConnector";
+import TimelineContent from "@mui/lab/TimelineContent";
+import TimelineOppositeContent from "@mui/lab/TimelineOppositeContent";
+import TimelineDot from "@mui/lab/TimelineDot";
+import { Event } from "@mui/icons-material";
 import Typography from '@mui/material/Typography';
 
 const TimeLine = ({ timelines = [] }) => {
   return (
     <div>
-      <Timeline position='alternate' >
+      <Timeline position='alternate'>
         {
           timelines.map((item, index) => (
             <TimelineItem key={index}>
@@ -22,7 +22,8 @@ const TimeLine = ({ timelines = [] }) => {
                 variant='body2'
                 color="text.secondary"
               >
-               {item.date.toString().split("T")[0]}
+                {/* Null checking for date */}
+                {item.date ? new Date(item.date).toLocaleDateString() : "No Date Provided"}
               </TimelineOppositeContent>
               <TimelineSeparator>
                 <TimelineConnector />
@@ -35,7 +36,6 @@ const TimeLine = ({ timelines = [] }) => {
                 <Typography variant='h6'>{item.title}</Typography>
                 <Typography>{item.description}</Typography>
               </TimelineContent>
-
             </TimelineItem>
           ))
         }
@@ -44,4 +44,4 @@ const TimeLine = ({ timelines = [] }) => {
   )
 }
 
-export default TimeLine
+export default TimeLine;
